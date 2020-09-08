9f4a0ee2360c011e1f6d7468d9c998f0e1eae572
Thamina33 <37588746+Thamina33@users.noreply.github.com>
2020-09-09 04:02:09 +0600

    job circular Id Added test3

--- app/src/main/java/com/metacoders/cakri/Job_prep_page.java
+++ app/src/main/java/com/metacoders/cakri/Job_prep_page.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Job_prep_page extends AppCompatActivity {
+public class Job_prep_page extends AppCompatActivity implements View.OnClickListener{
 
     CardView job_prep_card  , bcs_special  , all_Job_solution , Bcs ;
 
@@ -62,5 +62,72 @@ public class Job_prep_page extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
 
+
+            /* case R.id.bcs_special: // bcs special  card view
+
+                break;
+
+            case R.id.bankJobPrep: // bank job  card view
+
+                break;
+            case R.id.allJobSolution: // All Job Solution card view
+
+                break;  */
+            case R.id.career_guide: // Career Guide card view
+
+                break;
+            case R.id.protidin_news: // Everyday News card view
+
+                break;
+
+
+            case R.id.teacher_prp: // teacher job preparation card view
+
+                break;
+            case R.id.rcnt_qus_soln: // Recent Question Solution card view
+
+                break;
+            case R.id.trnslt_practice: // Translate Practice card view
+
+                break;
+
+            case R.id.focus_writing: // Focus Writing  card view
+
+                break;
+            case R.id.exclu_vuca: // Exclusive Vocabulary  card view
+
+                break;
+            case R.id.srt_tcnq: //  Shot-Cut Technique card view
+
+                break;
+            case R.id.xm_sgsn: // Exam Suggestion  card view
+
+                break;
+            case R.id.inspiration: // Inspirations card view
+
+                break;
+            case R.id.viva_exp: // Viva Experience   card view
+
+                break;
+            case R.id.interview_tips: //  Interview Tips card view
+
+                break;
+            case R.id.important_news: // Important News  card view
+
+                break;
+            case R.id.dwnld_zn: // Download Zone  card view
+
+                break;
+            case R.id.others: //  Others card view
+
+                break;
+
+            default:
+                break;
+        }
+    }
 }
