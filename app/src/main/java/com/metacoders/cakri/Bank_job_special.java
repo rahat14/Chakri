16cc44189fa64e2df04d1c3ff587a784c083649e
Thamina33 <37588746+Thamina33@users.noreply.github.com>
2020-09-11 03:30:14 +0600

    bcs & bank  Id Added test5

--- app/src/main/java/com/metacoders/cakri/Bank_job_special.java
+++ app/src/main/java/com/metacoders/cakri/Bank_job_special.java
@@ -7,10 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class Bank_job_special extends AppCompatActivity {
+public class Bank_job_special extends AppCompatActivity implements View.OnClickListener {
 
     TextView bottomText ;
-    CardView adviceBtn;
+    CardView adviceBtn,banglaBtn,engBtn,gkBtn,math,ictBtn;
 
 
     @Override
@@ -19,8 +19,65 @@ public class Bank_job_special extends AppCompatActivity {
         setContentView(R.layout.activity_bank_job_special);
 
         bottomText = findViewById(R.id.page_bottom_textView);
+        //id declaration
+        adviceBtn = findViewById(R.id.adviceBtn);
+        banglaBtn = findViewById(R.id.banglaBtn);
+        engBtn = findViewById(R.id.engBtn);
+        gkBtn = findViewById(R.id.gkBtn);
+        math = findViewById(R.id.math);
+        ictBtn = findViewById(R.id.ictBtn);
+
+
+        //click listener
+        adviceBtn.setOnClickListener(this);
+        banglaBtn.setOnClickListener(this);
+        engBtn.setOnClickListener(this);
+        gkBtn.setOnClickListener(this);
+        math.setOnClickListener(this);
+        ictBtn.setOnClickListener(this);
+
+
+
         bottomText.setText("★ব্যাংক লিখিত প্রস্তুতির সাজেশন দেখুন★");
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+
+        switch (v.getId()) {
+
+
+            case R.id.adviceBtn:
+
+                break;
+
+            case R.id.banglaBtn:
+
+                break;
+            case R.id.engBtn:
+
+                break;
+            case R.id.gkBtn:
+
+
+                break;
+            case R.id.math:
+
+                break;
+
+
+            case R.id.ictBtn:
+
+
+                break;
+
+
+            default:
+                break;
+        }
+
+
+    }
 }
--- app/src/main/java/com/metacoders/cakri/bcsSpecialPage.java
+++ app/src/main/java/com/metacoders/cakri/bcsSpecialPage.java
@@ -1,14 +1,97 @@
 package com.metacoders.cakri;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class bcsSpecialPage extends AppCompatActivity {
+public class bcsSpecialPage extends AppCompatActivity implements View.OnClickListener {
+
+    CardView adviceC,banglaC,englishC,bangladesh,internationl,geoC,gk_C,ict_C,math_logics,mental_skill,ethics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bcs_special_page);
+
+        //id declaration
+        adviceC = findViewById(R.id. adviceC);
+        banglaC = findViewById(R.id.banglaC);
+        englishC = findViewById(R.id.englishC);
+        bangladesh = findViewById(R.id.bangladesh);
+        internationl = findViewById(R.id.internationl);
+        geoC = findViewById(R.id.geoC);
+        gk_C = findViewById(R.id.gk_C);
+        ict_C = findViewById(R.id.ict_C);
+        math_logics = findViewById(R.id.math_logics);
+        mental_skill = findViewById(R.id.mental_skill);
+        ethics = findViewById(R.id.ethics);
+
+        //click listener
+        adviceC.setOnClickListener(this);
+        banglaC.setOnClickListener(this);
+        englishC.setOnClickListener(this);
+        bangladesh.setOnClickListener(this);
+        internationl.setOnClickListener(this);
+        geoC.setOnClickListener(this);
+        gk_C.setOnClickListener(this);
+        ict_C.setOnClickListener(this);
+        math_logics.setOnClickListener(this);
+        mental_skill.setOnClickListener(this);
+        ethics.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+
+            case R.id.adviceC:   //advise card View
+
+                break;
+
+            case R.id.banglaC:
+
+                break;
+            case R.id.englishC:
+
+                break;
+            case R.id.bangladesh:
+
+
+                break;
+            case R.id.internationl:
+
+                break;
+
+
+            case R.id.geoC:
+
+
+                break;
+            case R.id.gk_C:
+
+                break;
+
+            case R.id.ict_C:
+
+                break;
+            case R.id.math_logics:
+
+                break;
+            case R.id.mental_skill:
+
+
+                break;
+            case R.id.ethics:
+
+                break;
+
+            default:
+                break;
+        }
     }
 }
