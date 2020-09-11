d9efb485413ad6cc76358b6a92d1e3dfb60eee6b
Thamina33 <37588746+Thamina33@users.noreply.github.com>
2020-09-11 21:14:02 +0600

    age calculator 1

---
+++ app/src/main/java/com/metacoders/cakri/Result_age_calculator.java
@@ -0,0 +1,14 @@
+package com.metacoders.cakri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Result_age_calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_age_calculator);
+    }
+}
