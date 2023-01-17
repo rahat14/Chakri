6afaf4a6a4625b918106a575c29d1a2f0aebbcc0
Opuasad <opuasad02@gmail.com>
2023-01-16 22:44:07 -0600

    student-db update

--- Backend/chakri_admin/insert_course.php
+++ Backend/chakri_admin/insert_course.php
@@ -18,13 +18,58 @@ if (!isset($_SESSION['admin_email'])) {
     <title>Document</title>
 </head>
 <body>
+<!-- Main content -->
+<section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-6">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Student Details</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form>
 
-<h1>
-    asdfasdf
-</h1>
-    
-</body>
-</html>
+                <div class="card-body">
+
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Id</label>
+                    <input type="number" class="form-control" id="exampleInputEmail1" placeholder="Enter your id">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Name</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter your name">
+                  </div>
+                  <div class="form-group">
+                    <label for="phoneNumber">Phone</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter your phone number">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>
+                  <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
+            </div>
+            <!-- /.card -->
 
 
 
