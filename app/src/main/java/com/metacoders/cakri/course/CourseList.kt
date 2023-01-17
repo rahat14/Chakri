7b2471561450b8238b3ba34e5cf7b4d4483fc87d
Rahatul Aseaikin Shovo <rahatshovo6666@gmail.com>
2023-01-17 18:04:00 +0600

    added

---
+++ app/src/main/java/com/metacoders/cakri/course/CourseList.kt
@@ -0,0 +1,12 @@
+package com.metacoders.cakri.course
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.metacoders.cakri.R
+
+class CourseList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_list)
+    }
+}
